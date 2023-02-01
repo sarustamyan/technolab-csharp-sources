1f9b2312c413a3505c9b8a8057111b42a0364031
Sam Arustamyan <samvel.arustamyan@gmail.com>
2023-02-01 15:19:41 +0400

    Homework

---
+++ Homework/Lesson6/Diagonal/Program.cs
@@ -0,0 +1,16 @@
+﻿List<int> GetDiagonal(int[][] matrix)
+{
+    // TODO: implement the logic
+    throw new NotImplementedException();
+}
+
+var matrix = new[]
+{
+    new[] { 1, 2, 3 },
+    new[] { 4, 5, 6 },
+    new[] { 7, 8, 9 }
+};
+
+var diagonal = GetDiagonal(matrix);
+
+Console.WriteLine(string.Join(",", diagonal));
---
+++ README.md
@@ -0,0 +1,1 @@
+The docs for the course are located at [https://sarustamyan.github.io/technolab-csharp/](https://sarustamyan.github.io/technolab-csharp/)
