69382e0b821968685539f970226a27f46a6859f6
Sam Arustamyan <samvel.arustamyan@gmail.com>
2023-02-07 15:23:12 +0400

    Lesson3 - Classwork

---
+++ Lesson3/Classwork/Lowercase/Program.cs
@@ -0,0 +1,35 @@
+﻿var input = "Hello, World!";
+
+input.ToLower();
+
+for (int i = 0; i < input.Length; i++)
+{
+    char valod = input[i];
+    if ('A' <= valod && valod <= 'Z')
+    {
+        int asciiCode = (int)valod;
+        asciiCode += 'a' - 'A';
+        char lowercaseValod = (char)asciiCode;
+        Console.Write(lowercaseValod);
+    }
+    else
+    {
+        Console.Write(valod);
+    }
+}
+
+foreach (char valod in input)
+{
+    if ('A' <= valod && valod <= 'Z')
+    {
+        int asciiCode = (int)valod;
+        asciiCode += 'a' - 'A';
+        char lowercaseValod = (char)asciiCode;
+        Console.Write(lowercaseValod);
+    }
+    else 
+    {
+        Console.Write(valod);
+    }
+}
+
---
+++ Lesson3/Classwork/Prime/Program.cs
@@ -0,0 +1,53 @@
+﻿//int number = 6;
+//int i = 2;
+//for (i = 2; i < number; i++)
+//{
+
+//    if (number % i == 0)
+//    {
+//        Console.WriteLine(false);
+//        break;
+//    }    
+//}
+//if (i == number)
+//{
+//    Console.WriteLine(true);
+//}
+
+
+//Console.WriteLine(0.1 + 0.2);
+
+using Microsoft.VisualBasic;
+using System;
+
+Console.CursorVisible = false;
+Console.WriteLine("Task in progress...");
+Console.BackgroundColor = ConsoleColor.White;
+var number = 0;
+int k;
+
+
+
+for (int n = 0; n < 20; n++)
+{
+    Console.Write(" ");
+    Thread.Sleep(200);
+
+}
+
+
+
+for (int i = 5; i <= 100; i += 5)
+{
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.SetCursorPosition(21, Console.CursorTop);
+    Console.Write(i + "%");
+    
+    Thread.Sleep(200);
+}
+
+
+
+Console.WriteLine(" ");
+Console.Write("Finished!");
+Console.WriteLine(" ");
