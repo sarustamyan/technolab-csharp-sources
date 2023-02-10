da3275d41f1de56410cc147a59c37b2a27568fbd
Sam Arustamyan <samvel.arustamyan@gmail.com>
2023-02-10 14:36:54 +0400

    Lesson 3 Homework Answers

---
+++ Lesson4/HomeworkAnswers/Arrowhead/Program.cs
@@ -0,0 +1,11 @@
+﻿var n = 7;
+for (var i = 1; i <= n + 1; i = i + 2)
+{
+    Console.Write("".PadLeft((n - i) / 2, ' '));
+    Console.WriteLine("".PadLeft(i, '*'));
+}
+for (var i = 0; i < 3; i++)
+{
+    Console.Write("".PadLeft((n) / 2, ' '));
+    Console.WriteLine("*");
+}
---
+++ Lesson4/HomeworkAnswers/PiApproximation/Program.cs
@@ -0,0 +1,10 @@
+﻿var pi = 1d;
+for (var i = 1; i <= 500000; i++)
+{
+    if (i % 10000 == 0)
+    {
+        Console.WriteLine($"{i}: {pi * 4}");
+    }
+    var factor = (i % 2 == 0) ? 1 : -1;
+    pi = pi + factor * (1d / (i * 2 + 1));
+}
