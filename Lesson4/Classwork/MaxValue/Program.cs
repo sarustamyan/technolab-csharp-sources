683e97ff95a44ae1594e840a19737c1825fe0cfd
Sam Arustamyan <samvel.arustamyan@gmail.com>
2023-02-10 14:27:32 +0400

    Lesson4 - Classwork

---
+++ Lesson4/Classwork/MaxValue/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] items = new int[] { };
+
+var maxIndex = 0;
+
+for (var i = 0; i < items.Length; i++)
+{
+    if (items[maxIndex] <= items[i])
+    { 
+        maxIndex = i;
+    }
+}
+Console.WriteLine(maxIndex);
---
+++ Lesson4/Classwork/NumberOfSpaces/Program.cs
@@ -0,0 +1,11 @@
+﻿var text = "asdas dfsadf  as, sd";
+
+var quantity = 0;
+foreach (var character in text)
+{
+    if (character == ' ')
+    {
+        quantity++;
+    }
+}
+Console.WriteLine(quantity); 
