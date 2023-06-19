f99058c1cb66b0b217d7a7dde0a86a2a9225a04a
Sam Arustamyan <samvel.arustamyan@gmail.com>
2023-06-19 14:26:59 +0400

    Classwork - Lesson 5

---
+++ Classwork/Lesson5/Average/Program.cs
@@ -0,0 +1,12 @@
+﻿double Average(int[] items)
+{
+    int sum = 0;
+    foreach (int item in items)
+    {
+        sum += item;
+    }
+    return (double)sum / items.Length;
+}
+
+int[] param = new int[] { 1, 2, 3, 4, 10 };
+Console.WriteLine(Average(param));
---
+++ Classwork/Lesson5/DaysInMonth/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine(DaysInMonth(2, 2000));
+
+Console.WriteLine(DateTime.DaysInMonth(month: 2, year: 2000));
+
+int DaysInMonth(int month, int year)
+{
+    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+    {
+        return 31;
+    }
+    else if (month == 4 || month == 6 || month == 9 || month == 11)
+    {
+        return 30;
+    }
+    else if (month == 2)
+    {
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+        {
+            return 29;
+        }
+        else
+        {
+            return 28;
+        }
+    }
+    else
+    {
+        return -1;
+    }
+}
