8e8078f48c85242b28c404e5818e863d92925495
Sam Arustamyan <samvel.arustamyan@gmail.com>
2023-06-19 14:25:57 +0400

    HomeworkSolutions - Lesson 4

---
+++ HomeworkSolutions/Lesson4/LastPositive/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] items = new int[] { 1, 3, 5, -1, -6 };
+for (int i = items.Length - 1; i >= 0; i--)
+{
+    if (items[i] > 0)
+    {
+        Console.WriteLine(items[i]);
+        return;
+    }
+}
---
+++ HomeworkSolutions/Lesson4/Palindrome/Program.cs
@@ -0,0 +1,11 @@
+﻿string text = "racecar";
+
+for (int i = 0; i <= text.Length / 2; i++)
+{
+    if (text[i] != text[text.Length - 1 - i])
+    {
+        Console.WriteLine("false");
+        return;
+    }
+}
+Console.WriteLine("true");
