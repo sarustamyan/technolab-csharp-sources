e6689068c7b5c00b6351858451b1601120c5acd6
Sam Arustamyan <samvel.arustamyan@gmail.com>
2023-06-19 13:25:32 +0400

    HomeworkSolutions - Lesson 3

---
+++ HomeworkSolutions/Lesson3/ApproximationOfPi/Program.cs
@@ -0,0 +1,13 @@
+﻿double sum = 0;
+int factor = 1;
+int denom = 1;
+
+for (int i = 0; i < 5000000; i++)
+{
+    sum += (double)factor * 1 / denom;
+    factor *= -1;
+    denom += 2;
+}
+
+double pi = sum * 4;
+Console.WriteLine(pi);
---
+++ HomeworkSolutions/Lesson3/ProgressBar2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.CursorVisible = false;
+Console.WriteLine("Task in progress...");
+
+var length = 20;
+for (var i = 1; i <= length; i++)
+{
+    Console.BackgroundColor = ConsoleColor.White;
+    Console.Write("\r");
+    Console.Write("".PadRight(i));
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.Write("".PadRight(length - i));
+    Console.Write($" {i * (100 / length)}%");
+    Thread.Sleep(200);
+}
+
+Console.WriteLine();
+Console.WriteLine("Finished.");
