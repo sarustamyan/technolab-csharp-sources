498bff0c61713ef7325f3249a511c7403b1e519a
Sam Arustamyan <samvel.arustamyan@gmail.com>
2023-06-19 14:27:58 +0400

    Classwork - Lesson 6

---
+++ Classwork/Lesson6/Matrix/Program.cs
@@ -0,0 +1,16 @@
+﻿List<List<int>> matrix = new List<List<int>>();
+
+matrix.Add(new List<int> { 1, 2, 3 });
+matrix.Add(new List<int> { 4, 5, 6 });
+matrix.Add(new List<int> { 7, 8, 9 });
+
+int sum = 0;
+for (int i = 0; i < matrix.Count(); i++)
+{
+    for (int j = 0; j < matrix[i].Count(); j++)
+    {
+        sum += matrix[i][j];
+    }
+}
+
+Console.WriteLine(sum);
