a5445188a9412dd4e07a158a9910d2a85631fd56
Sam Arustamyan <samvel.arustamyan@gmail.com>
2024-12-06 16:27:33 +0400

    Practices

---
+++ Classwork/Lesson5Practice/Program.cs
@@ -0,0 +1,51 @@
+﻿//int[] a = new int[0] { };
+
+//int max = int.MinValue;
+//for (int i = 0; i < a.Length; i++)
+//{
+//    if (max < a[i])
+//    {
+//        max = a[i];
+//    }
+//}
+//Console.WriteLine(max);
+
+//string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+
+//int quantity = 0;
+//for (int i = 0; i < text.Length; i++)
+//{
+//    if (text[i] == ' ')
+//    {
+//        quantity++;
+//    }
+//}
+
+//Console.WriteLine(quantity);
+
+//string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+
+//int quantity = 0;
+//for (int i = 0; i < text.Length; i++)
+//{
+//    char ch = text[i];
+//    if (ch == ' ')
+//    {
+//        quantity++;
+//    }
+//}
+
+//Console.WriteLine(quantity);
+
+string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+
+int quantity = 0;
+foreach (var ch in text)
+{
+    if (ch == ' ')
+    {
+        quantity++;
+    }
+}
+
+Console.WriteLine(quantity);
---
+++ Classwork/ProgressBar/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.CursorVisible = false;
+Console.WriteLine("Task in progress...");
+
+int numberOfSteps = 20;
+
+for (int step = 1; step <= numberOfSteps; step++)
+{
+    Console.BackgroundColor = ConsoleColor.White;
+    for (int i = 0; i < step; i++)
+    {
+        Console.Write(" ");
+    }
+
+    Console.BackgroundColor = ConsoleColor.Black;
+    for (int i = 0; i < numberOfSteps - step; i++)
+    {
+        Console.Write(" ");
+    }
+    Console.Write(" " + step * 100 / numberOfSteps + "%\r");
+    Thread.Sleep(200);
+}
+Console.WriteLine("Finished!");
